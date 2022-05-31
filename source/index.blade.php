befa0e31ab377fa16ceb858bd958d13ff849ff8b
zurik <phillips@squirescompany.com>
2022-05-31 08:04:32 -0500

    Initial commit

---
+++ source/index.blade.php
@@ -0,0 +1,48 @@
+@extends('_layouts.main')
+
+@section('body')
+
+
+
+<div class="container-fluid px-4 py-5" id="custom-cards">
+    <!-- Page Title -->
+    <h2 class="pb-2 border-bottom">AOGR Banner's</h2>
+    <!-- Container START -->
+    <div class="container">
+      <!-- ROW START -->
+      <div class="row justify-content-center">
+        <!-- COL START -->
+        <div class="col-4">
+          <div class="card shadow-0 ">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+              <img src="/assets/images/AOGR-Banner-Desktop-320x250-JacksAgainstTheSky.gif" class="img-fluid" />
+              <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card shadow-0 ">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+              <img src="/assets/images/AOGR-Banner-Desktop-320x250-JacksAgainstTheSky.gif" class="img-fluid" />
+              <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card shadow-0 ">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+              <img src="/assets/images/AOGR-Banner-Desktop-320x250-JacksAgainstTheSky.gif" class="img-fluid" />
+              <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+              </a>
+            </div>
+          </div>
+        </div><!-- COL END -->
+      </div><!-- ROW END -->
+    </div><!-- Container END -->
+  </div>
+@endsection
